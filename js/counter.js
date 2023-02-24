b84136296a17eff777e7811eb37f50b9423ae882
marim99 <marim.khaled99@gmail.com>
2023-02-24 15:23:16 +0200

    cart #24

--- js/counter.js
+++ js/counter.js
@@ -3,14 +3,13 @@ let decrement = document.querySelector(".dec");
 let countText = document.querySelector(".counter__text");
 let count = 0;
 increment.addEventListener("click", () => {
-    count ++;
-    console.log(count);
-    countText.innerText = count;
+  count++;
+  console.log(count);
+  countText.innerText = count;
 });
-decrement.addEventListener("click", () => { 
-    count =count -1 ;
-    console.log(count);
-    count <= -1 ? count=0 : count;
-    countText.innerText = count
-   
-})
+decrement.addEventListener("click", () => {
+  count = count - 1;
+  console.log(count);
+  count <= -1 ? (count = 0) : count;
+  countText.innerText = count;
+});
