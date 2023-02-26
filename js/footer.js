8ede039ccf8994c00a6e6307d33e5346ed7598b4
Radwa Khalil <radwakhalil22@gmail.com>
2023-02-26 22:36:17 +0200

    user details

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-let container = document.querySelector('.all');
+let footerContainer = document.querySelector('.all');
 
 let footer = `<div class="row mb-0 section--4 section">
 <footer class="bg-dark text-center text-lg-start text-white">
@@ -229,4 +229,4 @@ let footer = `<div class="row mb-0 section--4 section">
 </div>
 <!--End of Products Page Container-->`
 
-container.insertAdjacentHTML("beforeend",footer);
+footerContainer.insertAdjacentHTML("beforeend",footer);
