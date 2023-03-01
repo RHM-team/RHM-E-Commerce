da81e7af57ac6d9611b5a0d26e5fb7c153b52a8d
marim99 <marim.khaled99@gmail.com>
2023-03-02 00:12:02 +0200

    fix #118

--- js/productSection.js
+++ js/productSection.js
@@ -6,7 +6,7 @@ var productSectionContainer = document.getElementById(
 );
 
 fetchData()
-  .then((data) => displaySampleOfProducts(data))
+  .then((data) => displaySamoleOfProducts(data))
   .catch((err) => console.log(err)); //Show Sample of Products
 
 function displaySamoleOfProducts(fetchProducts) {
