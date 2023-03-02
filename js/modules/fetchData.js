c847f18d90035e4636c3fd0c8d91429c70a89ee1
Hager Abd El Galil <Hager.galil66@gmail.com>
2023-03-02 19:44:02 +0200

    Loading Spinner

--- js/modules/fetchData.js
+++ js/modules/fetchData.js
@@ -1,6 +1,10 @@
 export default async function fetchData() {
+  var spinner = document.getElementById("spinner");
+  spinner.removeAttribute('hidden');
+  
   let response = await fetch("../data.json"); //fetch data by url
   let fetchedData = await response.text();
   let data = JSON.parse(fetchedData);
+  
   return data;
 }
