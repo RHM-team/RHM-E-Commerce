60be3c42149c209ccd0e2a178baf293f65771d58
hager khaled <hagerk720@gmail.com>
2023-03-03 22:18:32 +0200

    update

--- js/navbar.js
+++ js/navbar.js
@@ -124,7 +124,7 @@ let navbar = `<div class="row">
         <a
             class="col-12 col-md-auto nav-link d-flex justify-content-center align-items-md-center my-sm-3 my-md-0" href="../pages/productPage.html"
           >
-          <button class ="search__btn bg-dark border-dark" type="submit"><i class="fa fa-search search"></i></button>
+          
           </a>
           ${
             activeUser
